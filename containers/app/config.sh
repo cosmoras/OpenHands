5a3c47225e8dcd45d831c756a595f5609c55a4f7
cosmoras <cosmoresearcher@gmail.com>
2025-04-29 01:04:35 +0300

    Update config.sh

--- containers/app/config.sh
+++ containers/app/config.sh
@@ -2,3 +2,5 @@ DOCKER_REGISTRY=docker.io/cosmoras
 DOCKER_ORG=all-hands-ai
 DOCKER_IMAGE=openhands
 DOCKER_BASE_DIR="."
+# Runtime container image
+SANDBOX_RUNTIME_CONTAINER_IMAGE=docker.all-hands.dev/all-hands-ai/runtime:0.34-nikolaik
